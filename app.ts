e658493490285dfa59c6b65d081f62f1f79ce2ab
The Noah <thenoahbz@gmail.com>
2022-12-07 14:28:32 -0600

    chore: handle -h and --help flags

--- app.ts
+++ app.ts
@@ -9,6 +9,8 @@ switch (Deno.args[0]) {
     await moveEdited();
     break;
   case "help":
+  case "-h":
+  case "--help":
     console.log("Usage: deno run --allow-read --allow-write --allow-env app.ts [lightroom|edited]");
     console.log("  lightroom: Move Lightroom exports to the correct folders");
     console.log("  edited: Move edited files to the correct folders");
