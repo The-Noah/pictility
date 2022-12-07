3eb2563c35aacbb1bde07e109fde2192ccabaaa0
The Noah <thenoahbz@gmail.com>
2022-12-07 14:28:12 -0600

    chore: improve help message

--- app.ts
+++ app.ts
@@ -10,6 +10,10 @@ switch (Deno.args[0]) {
     break;
   case "help":
     console.log("Usage: deno run --allow-read --allow-write --allow-env app.ts [lightroom|edited]");
+    console.log("  lightroom: Move Lightroom exports to the correct folders");
+    console.log("  edited: Move edited files to the correct folders");
+    console.log("  help: Show this help message");
+    console.log("  (no argument): Move both Lightroom exports and edited files");
     break;
   default:
     await moveLightroomExports();
