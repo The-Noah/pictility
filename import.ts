7d9a984863cd59e9e87b26f113f67dde3e7723a3
The Noah <thenoahbz@gmail.com>
2022-12-07 15:48:58 -0600

    feat: scan all drives for files to import

--- import.ts
+++ import.ts
@@ -1,70 +1,87 @@
 import * as fs from "https://deno.land/std@0.167.0/fs/mod.ts";
 import * as path from "https://deno.land/std@0.167.0/path/mod.ts";
+import * as winStorage from "https://deno.land/x/win@0.2.0/api/Storage/mod.ts";
 import {PATH} from "./constants.ts";
 import exif from "./exif.ts";
-
-const SD_CARD_PATH = "D:\\DCIM";
 
 export default async function importFiles() {
   console.log("Importing files...");
   const start = Date.now();
 
+  const buffer = new Uint8Array(1024);
+  winStorage.FileSystem.GetLogicalDriveStringsW(1024, buffer);
+  const DRIVES = new Set(
+    String.fromCharCode(...buffer)
+      .split("\0")
+      .filter(Boolean)
+      .filter((drive) => drive.charCodeAt(0) >= 65 && drive.charCodeAt(0) <= 90)
+      .filter((drive) => drive !== "C")
+  );
+
   let count = 0;
-  for await (const entry of await fs.walk(SD_CARD_PATH)) {
-    if (!entry.isFile) {
+  for (const drive of DRIVES) {
+    const drive_path = path.join(`${drive}:`, "DCIM");
+
+    if (!(await fs.exists(drive_path))) {
       continue;
     }
 
-    const file_extension = path.extname(entry.path).toUpperCase();
+    for await (const entry of await fs.walk(drive_path)) {
+      if (!entry.isFile) {
+        continue;
+      }
 
-    if (![".JPG", ".CR2"].includes(file_extension)) {
-      continue;
+      const file_extension = path.extname(entry.path).toUpperCase();
+
+      if (![".JPG", ".CR2"].includes(file_extension)) {
+        continue;
+      }
+
+      const {model} = await exif(entry.path);
+
+      const date = (await Deno.stat(entry.path)).mtime;
+      const destination = `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, "0")}-${date.getDate().toString().padStart(2, "0")}`;
+      const destination_directory = path.join(PATH, destination, model);
+
+      // create directory structure
+      await fs.ensureDir(destination_directory);
+      await fs.ensureDir(path.join(destination_directory, "1. RAW"));
+      await fs.ensureDir(path.join(destination_directory, "2. JPEG"));
+      await fs.ensureDir(path.join(destination_directory, "3. Edited"));
+
+      const destination_path = path.join(destination_directory, file_extension === ".JPG" ? "2. JPEG" : "1. RAW", path.basename(entry.path));
+
+      if (await fs.exists(destination_path)) {
+        console.log(`File already exists: ${entry.name}`);
+        continue;
+      }
+
+      console.log(`Moving ${entry.name} to ${destination}...`);
+
+      await fs.copy(entry.path, destination_path);
+
+      // veriy file was copied
+      if (!(await fs.exists(destination_path))) {
+        console.log(`Failed to copy ${entry.name} to ${destination}`);
+        continue;
+      }
+
+      // verify the sha256 hash of the original and copied file are the same
+      const original_hash = await getFileHash(entry.path);
+      const copied_hash = await getFileHash(destination_path);
+
+      if (original_hash !== copied_hash) {
+        console.log(`Failed to copy ${entry.name} to ${destination}: hash mismatch`);
+        await Deno.remove(destination_path);
+
+        continue;
+      }
+
+      // delete original file
+      await Deno.remove(entry.path);
+
+      count++;
     }
-
-    const {model} = await exif(entry.path);
-
-    const date = (await Deno.stat(entry.path)).mtime;
-    const destination = `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, "0")}-${date.getDate().toString().padStart(2, "0")}`;
-    const destination_directory = path.join(PATH, destination, model);
-
-    // create directory structure
-    await fs.ensureDir(destination_directory);
-    await fs.ensureDir(path.join(destination_directory, "1. RAW"));
-    await fs.ensureDir(path.join(destination_directory, "2. JPEG"));
-    await fs.ensureDir(path.join(destination_directory, "3. Edited"));
-
-    const destination_path = path.join(destination_directory, file_extension === ".JPG" ? "2. JPEG" : "1. RAW", path.basename(entry.path));
-
-    if (await fs.exists(destination_path)) {
-      console.log(`File already exists: ${entry.name}`);
-      continue;
-    }
-
-    console.log(`Moving ${entry.name} to ${destination}...`);
-
-    await fs.copy(entry.path, destination_path);
-
-    // veriy file was copied
-    if (!(await fs.exists(destination_path))) {
-      console.log(`Failed to copy ${entry.name} to ${destination}`);
-      continue;
-    }
-
-    // verify the sha256 hash of the original and copied file are the same
-    const original_hash = await getFileHash(entry.path);
-    const copied_hash = await getFileHash(destination_path);
-
-    if (original_hash !== copied_hash) {
-      console.log(`Failed to copy ${entry.name} to ${destination}: hash mismatch`);
-      await Deno.remove(destination_path);
-
-      continue;
-    }
-
-    // delete original file
-    await Deno.remove(entry.path);
-
-    count++;
   }
 
   console.log(`Moved ${count} files in ${Date.now() - start}ms`);
